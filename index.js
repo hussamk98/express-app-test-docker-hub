a584c1e3fc3e6f24fb2e9f594511ec17c21c7a02
Hussam Khan <hussamkhan98@gmail.com>
2022-08-17 12:28:10 +0500

    update port

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 5000, () => {
 	console.log("Server started on port 3000");
 });
